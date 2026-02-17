085d2dcc22f109abc00b91ae384a7fa84edf55a9
Hwan seok Kim <kcsss4499@gmail.com>
2026-02-18 03:21:09 +0900

    streamlit cloud setting

--- llm.py
+++ llm.py
@@ -3,11 +3,24 @@ import os
 from dotenv import load_dotenv
 from langchain_upstage import UpstageEmbeddings
 from langchain_upstage import ChatUpstage
-from langchain_classic import hub
-from langchain_classic.chains import RetrievalQA
+from langchain_classic.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate,FewShotChatMessagePromptTemplate
 from langchain_pinecone import PineconeVectorStore
+from langchain_core.prompts import MessagesPlaceholder
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from config import answer_examples
+
+store = {}
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+
 
 
 def get_retrieval():
@@ -16,9 +29,31 @@ def get_retrieval():
     index='tax-index-markdown'
     database = PineconeVectorStore.from_existing_index(index_name=index, embedding=embeddings)
     retriever=database.as_retriever(search_kwargs={'k':4})
-    return get_retrieval
-    
+    return retriever
 
+def get_history_retrieval():
+      llm=get_llm()
+      retriever=get_retrieval()
+     
+      contextualize_q_system_prompt = (
+            "Given a chat history and the latest user question "
+            "which might reference context in the chat history, "
+            "formulate a standalone question which can be understood "
+            "without the chat history. Do NOT answer the question, "
+            "just reformulate it if needed and otherwise return it as is."
+        )
+
+      contextualize_q_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", contextualize_q_system_prompt),
+                MessagesPlaceholder("chat_history"),
+                ("human", "{input}"),
+            ]
+        )
+      history_aware_retriever=create_history_aware_retriever(
+             llm,retriever,contextualize_q_prompt
+        )    
+      return history_aware_retriever
 def get_llm():
     llm=ChatUpstage()
     return llm
@@ -41,27 +76,70 @@ def get_dictionary_chain():
     return dictionary_chain
 
 
-def get_ai_message(user_message):
+
+
+def get_rag_chain():
+      llm=get_llm()
+      example_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("human", "{input}"),
+        ("ai", "{answer}"),
+    ]
+)
+
+        # Few-shot 프롬프트 템플릿 생성
+      few_shot_prompt = FewShotChatMessagePromptTemplate(
+        example_prompt=example_prompt,
+        examples=answer_examples,
+)
+
+      
+      system_prompt = (
+        "당신은 소득세법 전문가입니다. 사용자의 소득세법에 관한 질문에 답변해주세요"
+        "아래에 제공된 문서를 활용해서 답변해주시고"
+        "답변을 알 수 없다면 모른다고 답변해주세요"
+        "답변을 제공할 때는 소득세법 (XX조)에 따르면 이라고 시작하면서 답변해주시고"
+        "2-3 문장정도의 짧은 내용의 답변을 원합니다"
+        "\n\n"
+        "{context}"
+    )
+    
+      qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            few_shot_prompt,
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+      history_aware_retriever=get_history_retrieval()
+      question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+      rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+      
+      
+      conversational_rag_chain = RunnableWithMessageHistory(
+            rag_chain,
+            get_session_history,
+            input_messages_key="input",
+            history_messages_key="chat_history",
+            output_messages_key="answer",
+        ).pick('answer')
+      return conversational_rag_chain
+
+
+def get_ai_response(user_message):
     # def  get_ai_messasge(user_message):
    
-# 환경변수를 불러옴
-    load_dotenv()
 
-
-    
-
-  
-       
-    
     dictionary_chain=get_dictionary_chain()
-    qa_chain=get_qa_chain()
-    tax_chain = {"query": dictionary_chain} | qa_chain
-    ai_message=tax_chain.invoke({"question" :user_message})
-    return ai_message["result"]
-
-def get_qa_chain():
-      llm=get_llm()
-      retriever=get_retrieval
-      prompt=hub.pull("rlm/rag-prompt")
-      qa_chain=RetrievalQA.from_chain_type(llm,retriever=retriever,chain_type_kwargs={"prompt":prompt} 
-    )
+    rag_chain=get_rag_chain()
+    tax_chain = {"input": dictionary_chain} | rag_chain
+    ai_response=tax_chain.stream(
+        {
+            "question" :user_message
+            },
+            config={
+                "configurable":{"session_id":"abc123"}
+            },)
+    return ai_response
